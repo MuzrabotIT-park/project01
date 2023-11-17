8ca360ab62dca96341ab1e1b87f520e2c00bfe3d
ozodbekPanjiyev <151119830+MuzrabotIT-park@users.noreply.github.com>
2023-11-17 21:28:28 +0500

    sss

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>hello world</h1>
+    <input type="text" placeholder="Search..."  > 
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+@import './assets/styles/main.scss'
+
+
+</style>
